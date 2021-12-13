f1e0c9051237e2bd93e16569d2fa7b9c578aacf7
Sergey <sergekuv@yandex.ru>
2021-12-13 21:27:36 +0300

    Add project files.

---
+++ PracticesTests/CalculatorTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Practices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Practices;
+
+namespace Practices.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void AdditionalTest()
+        {
+            Calculator calc = new();
+            Assert.IsTrue(calc.Additional(2,3) == 5);
+        }
+
+        [TestMethod()]
+        public void SubtractionTest()
+        {
+            Calculator calc = new();
+            Assert.AreEqual<int>(calc.Subtraction(2, 3), -1);
+        }
+
+        [TestMethod()]
+        public void MiltiplicationTest()
+        {
+            Calculator calc = new();
+            Assert.IsTrue (calc.Miltiplication(2, 3) == 6);
+        }
+
+        [TestMethod()]
+        public void DivisionTest_NormalCase()
+        {
+            Calculator calc = new();
+            Assert.IsTrue(calc.Division(7, 2) == 3);
+        }
+        [TestMethod()]
+
+        public void DivisionTest_ZeroCase()
+        {
+            Calculator calc = new();
+            Assert.ThrowsException<DivideByZeroException>(() => calc.Division(7, 0));
+        }
+
+    }
+}
+
---
+++ Practices_nUnit_Tests/CalculatorTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Practices;
+using NUnit.Framework;
+
+namespace Practices_NUnit_Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AdditionalTest()
+        {
+            Calculator calc = new();
+            Assert.IsTrue(calc.Additional(2, 3) == 5);
+        }
+
+        [Test]
+        public void SubtractionTest()
+        {
+            Calculator calc = new();
+            Assert.AreEqual(calc.Subtraction(2, 3), -1, 0.1);   //просто для разнообразия; 
+                                                                //использование double - не слишком хорошая идея для сравнения целых чисел
+        }
+
+        [Test]
+        public void MiltiplicationTest()
+        {
+            Calculator calc = new();
+            Assert.That(calc.Miltiplication(2, 3) == 6);
+        }
+
+        [Test]
+        public void DivisionTest_NormalCase()
+        {
+            Calculator calc = new();
+            Assert.IsTrue(calc.Division(7, 2) == 3);
+        }
+        [Test]
+
+        public void DivisionTest_ZeroCase()
+        {
+            Calculator calc = new();
+            Assert.Throws<DivideByZeroException>(() => calc.Division(7, 0));
+        }
+
+    }
+}
